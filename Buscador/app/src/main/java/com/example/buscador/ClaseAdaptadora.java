53734b306afca391d19665732b97cb057f56cfc9
Fatima Cordova <fatima150698@gmail.com>
2021-06-21 17:47:22 -0600

    Buscador en un RecyclerView

--- Buscador/app/src/main/java/com/example/buscador/ClaseAdaptadora.java
+++ Buscador/app/src/main/java/com/example/buscador/ClaseAdaptadora.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -14,9 +18,10 @@ import java.util.List;
 
 public class ClaseAdaptadora
         extends RecyclerView.Adapter<ClaseAdaptadora.ViewHolderRedes>
-        implements View.OnClickListener, View.OnLongClickListener {
+        implements Filterable, View.OnClickListener, View.OnLongClickListener {
 
     ArrayList<Redes> listaRedes;
+    ArrayList <Redes> filtrarNombre;
     private View.OnClickListener listener;
     private View.OnLongClickListener list;
 
@@ -45,7 +50,10 @@ public class ClaseAdaptadora
 
     @Override
     public void onBindViewHolder(ViewHolderRedes holder, int position) {
+        Redes currentItem = listaRedes.get(position);
+
         holder.txtNombre.setText(listaRedes.get(position).getNombre());
+        holder.imagen.setImageResource(currentItem.getImagen());
 
         if (Utilidades.visualizacion == Utilidades.LIST) {
             holder.imagen.setImageResource(listaRedes.get(position).getImagen());
@@ -80,15 +88,53 @@ public class ClaseAdaptadora
         return false;
     }
 
+    @Override
+    public Filter getFilter() {
+        return filtrar;
+    }
+
+    private Filter filtrar = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Redes> filtrarLista = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filtrarLista.addAll(listaRedes);
+            }
+            else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (Redes item : listaRedes) {
+                    if (item.getNombre().toLowerCase().contains(filterPattern)) {
+                        filtrarLista.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filtrarLista;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filtrarNombre.clear();
+            filtrarNombre.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
+
 
     public class ViewHolderRedes extends RecyclerView.ViewHolder {
 
         TextView txtNombre;
         ImageView imagen;
+        EditText edtBuscador;
+
 
         public ViewHolderRedes(View itemView) {
             super(itemView);
             txtNombre= (TextView) itemView.findViewById(R.id.txtRedes);
+            edtBuscador = (EditText) itemView.findViewById(R.id.edtBuscador);
             if (Utilidades.visualizacion==Utilidades.LIST){
                 imagen= (ImageView) itemView.findViewById(R.id.myImageView);
             }
--- Buscador/app/src/main/java/com/example/buscador/MainActivity.java
+++ Buscador/app/src/main/java/com/example/buscador/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     ClaseAdaptadora adaptador;
     RecyclerView recycler;
     EditText edtBuscador;
-    Button btnBuscar;
 
     ArrayList <Redes> filtrarNombre;
 
@@ -27,68 +29,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        recycler = (RecyclerView) findViewById(R.id.recycler);
         edtBuscador = (EditText) findViewById(R.id.edtBuscador);
-        btnBuscar = (Button) findViewById(R.id.btnBuscar);
-
-
+        recycler();
         filtrarNombre = new ArrayList<>();
-        filtrarNombre = llenarLista();
-
-        adaptador = new ClaseAdaptadora(filtrarNombre);
-        recycler.setAdapter(adaptador);
-        recycler.setLayoutManager(new LinearLayoutManager(this));
+        llenarLista();
+        buscar();
 
         edtBuscador.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
+        }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-            }
+        }
 
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                filtrarNombre.clear();
-                filtrarNombre = llenarLista();
-                if(s.toString().isEmpty()){
-                    recycler.setAdapter(new ClaseAdaptadora(filtrarNombre));
-                }
-                else{
-                    search(s.toString());
-                }
-               adaptador.notifyDataSetChanged();
-            }
-        });
+        @Override
+        public void afterTextChanged(Editable s) {
+           adaptador.getFilter().filter(s);
+        }
+    });
+}
+    private  void llenarLista() {
+        filtrarNombre = new ArrayList<>();
+        filtrarNombre.add(new Redes("Discord",R.drawable.discord));
+        filtrarNombre.add(new Redes("Facebook",R.drawable.facebook));
+        filtrarNombre.add(new Redes("Instagram",R.drawable.instagram));
+        filtrarNombre.add(new Redes("Pinterest",R.drawable.pinterest));
+        filtrarNombre.add(new Redes("Snapchat",R.drawable.snapchat));
+        filtrarNombre.add(new Redes("Telegram",R.drawable.telegram));
+        filtrarNombre.add(new Redes("Twitter",R.drawable.twitter));
+        filtrarNombre.add(new Redes("Whatsapp",R.drawable.whatsapp));
+        filtrarNombre.add(new Redes("YouTube",R.drawable.youtube));
     }
 
-    private  ArrayList<Redes> llenarLista() {
-        ArrayList<Redes> redS = new ArrayList<>();
-        redS.add(new Redes("Discord",R.drawable.discord));
-        redS.add(new Redes("Facebook",R.drawable.facebook));
-        redS.add(new Redes("Instagram",R.drawable.instagram));
-        redS.add(new Redes("Pinterest",R.drawable.pinterest));
-        redS.add(new Redes("Snapchat",R.drawable.snapchat));
-        redS.add(new Redes("Telegram",R.drawable.telegram));
-        redS.add(new Redes("Twitter",R.drawable.twitter));
-        redS.add(new Redes("Whatsapp",R.drawable.whatsapp));
-        redS.add(new Redes("YouTube",R.drawable.youtube));
-        return redS;
-    }
 
-    private void buscar(String red){
+    private void buscar() {
         recycler.setAdapter(new ClaseAdaptadora(filtrarNombre));
         adaptador.notifyDataSetChanged();
     }
 
-    private void search(String busqueda){
-        filtrarNombre.stream().filter(redes -> redes.getNombre()=="Discord");
-        recycler.setAdapter(new ClaseAdaptadora(filtrarNombre));
-        adaptador.notifyDataSetChanged();
+    private void recycler(){
+        recycler = (RecyclerView) findViewById(R.id.recycler);
+
+        if (Utilidades.visualizacion == Utilidades.LIST) {
+            recycler.setLayoutManager(new LinearLayoutManager(this));
+        }
+
+        recycler.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        adaptador = new ClaseAdaptadora(filtrarNombre);
+
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(adaptador);
+
     }
 }
